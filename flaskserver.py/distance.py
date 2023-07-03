25f36c986af583790e3eb411586c76ae518a880f
ramzd18 <rbpeddu@gmail.com>
2023-07-02 21:12:24 -0400

    bug fixes

--- flaskserver.py/distance.py
+++ flaskserver.py/distance.py
@@ -9,6 +9,7 @@ def distanceBetween(home,away):
     away_lang=geolocator.geocode(away).latitude
     return distance.geodesic((home_lat,home_lang),(away_lat,away_lang))
 def distanceUS(name):
+   geolocator = ArcGIS(scheme="https")
    name_lat= geolocator.geocode(name).longitude
    name_long=geolocator.geocode(name).latitude
    return distance.geodesic((name_lat,name_long),(38.9072,77.0369))
