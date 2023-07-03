b1b4bd7287ecde348a0eab1f2f500169a61de5c8
ramzd18 <rbpeddu@gmail.com>
2023-07-02 20:25:39 -0400

    Fnishing API data retrieval

--- flaskserver.py/backend.py
+++ flaskserver.py/backend.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import distance
 import socialmediadata
-
+import oddsapi
 app = Flask(__name__)
 
 @app.route('/ufcmodel')
@@ -9,4 +9,17 @@ def ufc_model(fighter1, fighter2, location):
     distanceto=distance.distanceUS(location)
     fighter1social= socialmediadata.socialmediaData(fighter1)
     fighter2social=socialmediadata.socialmediaData(fighter2)
-    
+    list= oddsapi.ufcOdds(fighter1,fighter2)
+    if(list[0]=="error"):
+        return "error"
+    if(list[0].equals(fighter1)):
+        fighter1odds=list[1]
+        fighter2odds=list[3]
+    else:
+        fighter1odds=list[3]
+        fighter2odds=list[1]
+
+def nfl_model(team1,team2):
+    team1social=socialmediadata.socialmediaData(team1)    
+    team2social=socialmediadata.socialmediaData(team2)  
+  
